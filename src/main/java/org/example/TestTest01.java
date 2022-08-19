f32635469c28478210ebfafa7263dc58f6b98756
mhh <1216409494@qq.com>
2022-08-20 00:43:56 +0800

    aa

---
+++ src/main/java/org/example/TestTest01.java
@@ -0,0 +1,11 @@
+package main.java.org.example;
+
+import org.testng.annotations.Test;
+
+public class TestTest01 {
+    // C1
+    @Test
+    public void Test_Case_Title() {
+      System.out.println("asdfa");
+    }
+}
---
+++ testsuit01.t.md
@@ -0,0 +1,11 @@
+# testsuit01
+
+* 1 Test Case Title
+* 2 Test Case Title
+* 3 Test Case Title
+* 4 Test Case Title
+* 5 Test Case Title
+    * Step 1
+    * Step 1
+    * Step 2
+    * Step 3
